6929e591b8ac29c65bce6b8268cd3440765153c4
Marisa Doig <mcr.doig@gmail.com>
2020-09-24 03:08:27 +0000

    Implement user authentication with bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  resources :users, only: [:create, :new]
+  resources :sessions, only: [:create, :new, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
